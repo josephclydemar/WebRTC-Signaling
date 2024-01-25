7ec4fa41f77eaaed3f246ad1b378849054aa79b8
Joseph Clyde Mar <clydemar234@gmail.com>
2024-01-26 00:51:49 +0800

    refactor(client): Remove function parameters 'getVideo' and 'getAudio' from 'getLocalMediaStream'

--- src/client/private/userMedia.ts
+++ src/client/private/userMedia.ts
@@ -1,8 +1,8 @@
-async function getLocalMediaStream(getVideo: boolean, getAudio: boolean): Promise<MediaStream> {
+async function getLocalMediaStream(): Promise<MediaStream> {
     const localVideo: HTMLVideoElement = document.getElementById('local-video') as HTMLVideoElement;
     let localMediaStream: MediaStream = await navigator.mediaDevices.getUserMedia({
-        video: getVideo,
-        audio: getAudio,
+        video: true,
+        audio: true,
     });
     localVideo.srcObject = localMediaStream;
     return localMediaStream;
