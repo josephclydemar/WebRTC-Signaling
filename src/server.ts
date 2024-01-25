a027947b7840431d5ea64a0ac4db27cefa83c61d
Joseph Clyde Mar <clydemar234@gmail.com>
2024-01-26 02:38:10 +0800

    feat(server): Add http server setup for production environment while https server is for development environment

--- src/server.ts
+++ src/server.ts
@@ -12,10 +12,7 @@ dotenv.config();
 
 const app: Express = express();
 const PORT: string = process.env.SERVER_PORT as string;
-// const KEY: string = process.env.HTTPS_KEY as string;
-// const CERT: string = process.env.HTTPS_CERT as string;
-const KEY: Buffer = fs.readFileSync('./certs/cert.key');
-const CERT: Buffer = fs.readFileSync('./certs/cert.crt');
+const NODE_ENVIRONMENT: string = process.env.NODE_ENV as string;
 
 let clients: string[] = [];
 
@@ -36,65 +33,133 @@ app.all('*', function (req: Request, res: Response): void {
     console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t${req.protocol}  ${req.method} ${req.url}`);
 });
 
-const httpsServer = https.createServer({ key: KEY, cert: CERT }, app);
-httpsServer.listen(PORT, function (): void {
-    console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tListening on port ${PORT}`);
-});
 
-const io = new Server(httpsServer, {
-    cors: { origin: '*' },
-});
+if(NODE_ENVIRONMENT === 'development') {
+    const KEY: Buffer = fs.readFileSync('./certs/cert.key');
+    const CERT: Buffer = fs.readFileSync('./certs/cert.crt');
+    const httpsServer = https.createServer({ key: KEY, cert: CERT }, app);
 
-io.on('connection', function (socket) {
-    console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tNew client ${socket.id} has connected`);
-    clients.push(socket.id);
-    socket.emit(
-        'for_me',
-        clients.filter(function (item: string): boolean {
-            return item !== socket.id;
-        }),
-    );
-
-    socket.broadcast.emit('new_client', clients);
-
-    socket.on('message', function (data: string): void {
-        console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tClient ${socket.id} message: ${data}`);
+    httpsServer.listen(PORT, function (): void {
+        console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tListening on port ${PORT}`);
+    });
+    const io = new Server(httpsServer, {
+        cors: { origin: '*' },
     });
 
-    socket.on('rtc_sdp_offer', function (data: SDP): void {
-        const { sendTo } = data;
-        socket.to(sendTo).emit('rtc_sdp_offer_pass', data);
-        console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `SDP-> SendFrom: ${data.sendFrom}\tType: ${data.type}`);
-    });
+    io.on('connection', function (socket) {
+        console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tNew client ${socket.id} has connected`);
+        clients.push(socket.id);
+        socket.emit(
+            'for_me',
+            clients.filter(function (item: string): boolean {
+                return item !== socket.id;
+            }),
+        );
 
-    socket.on('rtc_sdp_answer', function (data: SDP): void {
-        const { sendTo } = data;
-        socket.to(sendTo).emit('rtc_sdp_answer_pass', data);
-        console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `SDP-> SendFrom: ${data.sendFrom}\tType: ${data.type}`);
-    });
+        socket.broadcast.emit('new_client', clients);
 
-    socket.on('rtc_ready_for_remote_ice', function (data: any): void {
-        const { sendTo } = data;
-        socket.to(sendTo).emit('rtc_ready_for_remote_ice_pass', data);
-    });
-
-    socket.on('rtc_ice_offer', function (data: ICECollection): void {
-        const { sendTo } = data;
-        socket.to(sendTo).emit('rtc_ice_offer_pass', data);
-        console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `ICE-> SendFrom: ${data.sendFrom}\tType: ${data.type}`, data.ice);
-    });
-
-    socket.on('rtc_ice_answer', function (data: ICECollection): void {
-        const { sendTo } = data;
-        socket.to(sendTo).emit('rtc_ice_answer_pass', data);
-        console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `ICE-> SendFrom: ${data.sendFrom}\tType: ${data.type}`, data.ice);
-    });
-
-    socket.on('disconnect', function () {
-        console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tClient ${socket.id} has disconnected`);
-        clients = clients.filter(function (item: string): boolean {
-            return item !== socket.id;
+        socket.on('message', function (data: string): void {
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tClient ${socket.id} message: ${data}`);
         });
-        socket.broadcast.emit('client_disconnect', clients);
+
+        socket.on('rtc_sdp_offer', function (data: SDP): void {
+            const { sendTo } = data;
+            socket.to(sendTo).emit('rtc_sdp_offer_pass', data);
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `SDP-> SendFrom: ${data.sendFrom}\tType: ${data.type}`);
+        });
+
+        socket.on('rtc_sdp_answer', function (data: SDP): void {
+            const { sendTo } = data;
+            socket.to(sendTo).emit('rtc_sdp_answer_pass', data);
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `SDP-> SendFrom: ${data.sendFrom}\tType: ${data.type}`);
+        });
+
+        socket.on('rtc_ready_for_remote_ice', function (data: any): void {
+            const { sendTo } = data;
+            socket.to(sendTo).emit('rtc_ready_for_remote_ice_pass', data);
+        });
+
+        socket.on('rtc_ice_offer', function (data: ICECollection): void {
+            const { sendTo } = data;
+            socket.to(sendTo).emit('rtc_ice_offer_pass', data);
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `ICE-> SendFrom: ${data.sendFrom}\tType: ${data.type}`, data.ice);
+        });
+
+        socket.on('rtc_ice_answer', function (data: ICECollection): void {
+            const { sendTo } = data;
+            socket.to(sendTo).emit('rtc_ice_answer_pass', data);
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `ICE-> SendFrom: ${data.sendFrom}\tType: ${data.type}`, data.ice);
+        });
+
+        socket.on('disconnect', function () {
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tClient ${socket.id} has disconnected`);
+            clients = clients.filter(function (item: string): boolean {
+                return item !== socket.id;
+            });
+            socket.broadcast.emit('client_disconnect', clients);
+        });
     });
-});
+} else if(NODE_ENVIRONMENT === 'production') {
+    const httpServer = app.listen(PORT, function (): void {
+        console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tListening on port ${PORT}`);
+    });
+
+    const io = new Server(httpServer, {
+        cors: { origin: '*' },
+    });
+
+    io.on('connection', function (socket) {
+        console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tNew client ${socket.id} has connected`);
+        clients.push(socket.id);
+        socket.emit(
+            'for_me',
+            clients.filter(function (item: string): boolean {
+                return item !== socket.id;
+            }),
+        );
+
+        socket.broadcast.emit('new_client', clients);
+
+        socket.on('message', function (data: string): void {
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tClient ${socket.id} message: ${data}`);
+        });
+
+        socket.on('rtc_sdp_offer', function (data: SDP): void {
+            const { sendTo } = data;
+            socket.to(sendTo).emit('rtc_sdp_offer_pass', data);
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `SDP-> SendFrom: ${data.sendFrom}\tType: ${data.type}`);
+        });
+
+        socket.on('rtc_sdp_answer', function (data: SDP): void {
+            const { sendTo } = data;
+            socket.to(sendTo).emit('rtc_sdp_answer_pass', data);
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `SDP-> SendFrom: ${data.sendFrom}\tType: ${data.type}`);
+        });
+
+        socket.on('rtc_ready_for_remote_ice', function (data: any): void {
+            const { sendTo } = data;
+            socket.to(sendTo).emit('rtc_ready_for_remote_ice_pass', data);
+        });
+
+        socket.on('rtc_ice_offer', function (data: ICECollection): void {
+            const { sendTo } = data;
+            socket.to(sendTo).emit('rtc_ice_offer_pass', data);
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `ICE-> SendFrom: ${data.sendFrom}\tType: ${data.type}`, data.ice);
+        });
+
+        socket.on('rtc_ice_answer', function (data: ICECollection): void {
+            const { sendTo } = data;
+            socket.to(sendTo).emit('rtc_ice_answer_pass', data);
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\t`, `ICE-> SendFrom: ${data.sendFrom}\tType: ${data.type}`, data.ice);
+        });
+
+        socket.on('disconnect', function () {
+            console.log(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}   ${v4()}\tClient ${socket.id} has disconnected`);
+            clients = clients.filter(function (item: string): boolean {
+                return item !== socket.id;
+            });
+            socket.broadcast.emit('client_disconnect', clients);
+        });
+    });
+
+}
